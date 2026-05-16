36289979370f645856526dec3d7230609ee77587
love-quinn <lucaslucal75@gmail.com>
2026-05-16 13:25:31 -0300

    Updates

---
+++ precache-manifest.1e7805d70554b100476624ee97fd773c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "./e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "url": "./f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "url": "./f721466883998665b87923b92dea655b.svg"
+  },
+  {
+    "revision": "48808977e754b025940c49c0d784e751",
+    "url": "./index.html"
+  },
+  {
+    "url": "./main.2811a66737a4db4062df.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.bc4e625a162511e2d2d47e123340e2da.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("./precache-manifest.1e7805d70554b100476624ee97fd773c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
