f1404731b066bb8448aac2d9f12eea222c297916
love-quinn <lucaslucal75@gmail.com>
2026-05-16 13:47:58 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.1e7805d70554b100476624ee97fd773c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("./precache-manifest.e772995943afb2a8555f4cb4de2a3cb0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
